170db39b5c7038f5d1dd65b9ff6f19d79bffb1c3
Guilherme Teixeira Santos <guilherme.teixeira@tsa.local>
2022-11-22 06:47:30 -0300

    Conexão Banco
    Alteração na senha e hostname

--- teste.php
+++ teste.php
@@ -1,8 +1,8 @@
 <?php
 $banco = "mundo169_iqf";
 $usuario = "mundo169";
-$senha = '"Ed$on192"';
-$hostname = 'localhost:3306';
+$senha = 'Ed$on192';
+$hostname = "localhost:3306";
 $conn = mysql_connect($hostname,$usuario,$senha); mysql_select_db($banco) or die( “Não foi possível conectar ao banco MySQL”);
 if (!$conn) {echo “Não foi possível conectar ao banco MySQL.
 “; exit;}
