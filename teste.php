e18fb95f83eeddbf84a5908cafab8081dbb3ad33
Guilherme Teixeira Santos <guilherme.teixeira@tsa.local>
2023-03-28 13:33:47 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -1,12 +1,16 @@
 <?php
 $banco = "mundo169_iqf";
 $usuario = "mundo169";
-$senha = 'Ed$on192';
+$senha = '"Ed$on192"';
 $hostname = "localhost:3306";
-$conn = mysql_connect($hostname,$usuario,$senha); mysql_select_db($banco) or die( “Não foi possível conectar ao banco MySQL”);
-if (!$conn) {echo “Não foi possível conectar ao banco MySQL.
-“; exit;}
-else {echo “Parabéns!! A conexão ao banco de dados ocorreu normalmente!.
-“;}
+$conn = mysql_connect($hostname,$usuario,$senha);
+mysql_select_db($banco) or die( “Não foi possível conectar ao banco MySQL”);
+if(!$conn){
+  echo “Não foi possível conectar ao banco MySQL.“;
+  exit;
+}
+else {
+  echo “Parabéns!! A conexão ao banco de dados ocorreu normalmente!.“;
+}
 mysql_close();
 ?>
